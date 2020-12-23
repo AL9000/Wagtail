d118d1fe0623ddf70b2f80b6363859210ee53903
jmr <jmr@makina-corpus.com>
2020-12-23 23:58:04 +0100

    Add python-decouple and remove SECRET_KEY from settings

--- AL_9000/settings/dev.py
+++ AL_9000/settings/dev.py
@@ -3,9 +3,6 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'qi2&j=_=*+vq4u$ik^d(^e7!m^x8l*$(v*kz%g96!y6o!m+_4+'
-
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*'] 
 
